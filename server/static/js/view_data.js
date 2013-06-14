69e652743695c0cce166c91cea9949037803641f
Alexander James Wallar <aw204@st-andrews.ac.uk>
2013-06-14 01:20:28 -0400

    Nightly build. Read the latest blog post for a detailed description

--- server/static/js/view_data.js
+++ server/static/js/view_data.js
@@ -3,14 +3,36 @@ var email = undefined;
 
 var password = undefined;
 
+var latestSearch = undefined;
+
 window.onload = function () {
 	$("#loginMessage").modal("show");
+}
+
+function graphEnabled() {
+	if ($("#graphical").attr("class") != "disabled") {
+		$("#graphical").attr("class", "active");
+		$("#tabular").attr("class", "");
+		searchData(latestSearch);
+	}
+}
+
+function tableEnabled() {
+	if ($("#graphical").attr("class") != "disabled") {
+		$("#graphical").attr("class", "");
+		$("#tabular").attr("class", "active");
+		searchData(latestSearch);
+	}
+}
+
+function getNames() {
+	$.getJSON("/get_names/" + email + "/" + password, nameWork);
 }
 
 function getData () {
 	email = $("#proctorEmail").val();
 	password = $("#password").val();
-	$.getJSON("/get_names/" + email + "/" + password, nameWork);
+	$.getJSON("/get_names/" + email + "/" + password, getNames());
 }
 
 function nameWork(nameData) {
@@ -18,13 +40,30 @@ function nameWork(nameData) {
 	for (var i = 0; i < nameData.length; i++) {
 		names.push(makeStringPresentable(nameData[i]["fName"]) + " " + makeStringPresentable(nameData[i]["lName"]));
 	}
-	//alert(name.concat(emails));
+
+	names.sort();
 	$("#searchTerm").typeahead().data('typeahead').source = names;
-	//alert($("#searchTerm").typeahead().data('typeahead').source);
+
+	var nameTable = new Table("<h2>Subjects</h2>");
+	for (var i = 0; i < names.length; i++) {
+		nameTable.addRow(["<a class=\"lead\" href=\"#\" onclick='searchData(\"" + 
+			names[i].toLowerCase() + "\")'>" + names[i] + "</a>"]);
+	}
+
+	$("#graphical").attr("class", "disabled");
+	$("#tabular").attr("class", "disabled");
+	$("#dataHolder").html("");
+	$("#nameArea").html(nameTable.getHtml());
 }
 
 // clean up the string more
 function searchData(searchTerm) {
+	if ($("#graphical").attr("class") == "disabled") {
+		$("#graphical").attr("class", "");
+		$("#tabular").attr("class", "active");
+	}
+
+	latestSearch = searchTerm;
 	$("#searchTerm").val("");
 	var terms = searchTerm.split(" ");
 	if (terms.length == 1) {
@@ -77,6 +116,112 @@ function visualizeData(data) {
 	$("#nameArea").html(
 		"<h2>" + makeStringPresentable(data[0]["fName"]) + " " + 
 		makeStringPresentable(data[0]["lName"]) + "</h2>");
+
+	if($("#tabular").attr("class") == "active") {
+		tableVisualization(results);
+	} else if ($("#graphical").attr("class") == "active") {
+		graphicVisualization(results);
+	}
+	$(".collapse").collapse("hide");
+}
+
+function Chart(title, containerId) {
+	this.title = title;
+	this.series = {};
+	this.categories = new Array();
+	$("#" + containerId).append('<div class="chart" id="' + title + 
+		'" style="min-width: 500px; height: 300px; margin: 0 auto"></div>');
+}
+
+Chart.prototype.pushCategory = function (cat) {
+	this.categories.push(cat);
+}
+
+Chart.prototype.pushSeries = function (key, value) {
+	if (this.series[key] == undefined){
+		this.series[key] = new Array();
+	}
+	this.series[key].push(value);
+}
+
+Chart.prototype.compileSeries = function () {
+	var seriesArray = new Array();
+	for (var i in this.series) {
+		seriesArray.push({name : i, data : this.series[i]});
+	}
+	return seriesArray;
+}
+
+Chart.prototype.show = function (yTitle, yUnit) {
+	var seriesArray = this.compileSeries();
+	//alert(JSON.stringify(seriesArray));
+	$("#" + this.title).highcharts({
+        chart: {
+            type: 'line',
+            marginRight: 130,
+            marginBottom: 40
+        },
+        title: {
+            text: this.title,
+            x: -20 //center
+        },
+        xAxis: {
+            categories: this.categories
+        },
+        yAxis: {
+        	title: {
+        		text : yTitle
+        	},
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        tooltip: {
+        	valueSuffix : " " + yUnit
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'top',
+            x: -10,
+            y: 100,
+            borderWidth: 0
+        },
+        series: seriesArray
+    });
+}
+
+function graphicVisualization(results) {
+
+	var balanceChart = new Chart("Balance", "dataHolder");
+	var memoryChart = new Chart("Memory", "dataHolder");
+	var reflexChart = new Chart("Reflex", "dataHolder");
+
+	for (var i = 0; i < results.length; i++) {
+
+		balanceChart.pushCategory(results[i]["time"].slice(4, 21));
+		memoryChart.pushCategory(results[i]["time"].slice(4, 21));
+		reflexChart.pushCategory(results[i]["time"].slice(4, 21));
+
+		balanceChart.pushSeries("Within Boundary", results[i]["balance"]["percent"]);
+
+		memoryChart.pushSeries("Initial Deviation", results[i]["memory"]["initialDev"]);
+		memoryChart.pushSeries("Final Deviation", results[i]["memory"]["finalDev"]);
+		memoryChart.pushSeries("Velocity", results[i]["memory"]["velocity"]);
+
+		reflexChart.pushSeries("Left", results[i]["reflex"]["circleA"]["percent"]);
+		reflexChart.pushSeries("Right", results[i]["reflex"]["circleB"]["percent"]);
+	}
+
+	balanceChart.show("Percent %", "%");
+	reflexChart.show("Percent %", "%");
+	memoryChart.show("Scaled Pixel Metric", "SPM");
+
+}
+
+function tableVisualization(results) {
 	for (var i = 0; i < results.length; i++) {
 
 		$("#dataHolder").append(addTab(results[i]["time"]));
@@ -108,6 +253,5 @@ function visualizeData(data) {
 		$("#" + innerId).append(reflexTable.getHtml());
 
 	}
-	$(".collapse").collapse("hide");
 }
 
