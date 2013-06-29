Fixed the bug with the id assignment. Now it works. Also, added a visualization of the average looking point with both of the eyes. This is in calibration.py

img_proc/eyetracker/blob.py
@@ -15,8 +15,13 @@ class Blob:
         self.contourArea = contourArea
         self.Point = namedtuple("Point", "x y")
 
+    # IMPORTANT: returns the centroid for the given size
+    # of the sub image
     def getCentroid(self):
-        retCentroid = self.Point(self.centroid[0], self.centroid[1])
+        retCentroid = self.Point(
+            self.centroid[0], 
+            self.centroid[1]
+        )
         return retCentroid
 
     def getConvexHull(self):
@@ -32,8 +37,11 @@ class Blob:
         return self.contourArea 
 
 def getBlobs(BW, minSize, maxSize = None):
-    cs, _ = cv2.findContours(BW.astype(np.uint8), mode = cv2.RETR_EXTERNAL, 
-        method = cv2.CHAIN_APPROX_SIMPLE)
+    cs, _ = cv2.findContours(
+        BW.astype(np.uint8), 
+        mode = cv2.RETR_EXTERNAL, 
+        method = cv2.CHAIN_APPROX_SIMPLE
+    )
     blobList = list()
     for con in cs:
         if maxSize == None:
@@ -50,9 +58,18 @@ def getBlobs(BW, minSize, maxSize = None):
 
                 cHull = cv2.convexHull(con)
 
-                blobList += [Blob((int(m10/m00), int(m01/m00)),
-                    cHull, cv2.contourArea(cHull),
-                    con, cv2.contourArea(con))]
+                blobList += [
+                    Blob(
+                        (
+                            int(m10/m00), 
+                            int(m01/m00)
+                        ),
+                        cHull, 
+                        cv2.contourArea(cHull),
+                        con, 
+                        cv2.contourArea(con)
+                    )
+                ]
             except:
 				pass
     return blobList

img_proc/eyetracker/calibration.py
@@ -3,73 +3,83 @@ import eyetracker
 import cv2
 import numpy as np
 from movingaverage import MovingAveragePoints
+from collections import namedtuple
 
 class EyeCalibration:
 
 	def __init__(self):
+		self.Point = namedtuple("Point", "x y")
 		self.camera = cv2.VideoCapture(0)
 		self.tracker = eyetracker.EyeTracker()
 		self.movAvgDict = dict()
-		self.movAvgLength = 13
+		self.lookingPointMovAvg = MovingAveragePoints(
+			self.Point(
+				self.tracker.xScale / 2,
+				self.tracker.yScale / 2
+			),
+			4
+		)
+		self.subDict = dict()
+		self.movAvgLength = 4
 
-	def drawCanvas(self):
-		canvas = np.zeros((480, 640))
-		for key in self.movAvgDict:
-			try:
-				"""
-				cv2.circle(
-					canvas,
-					(
-						self.movAvgDict[key]["centroid"].getLastCompoundedResult().x,
-						self.movAvgDict[key]["centroid"].getLastCompoundedResult().y
-					),
-					5, 255, 5
-				)
-				"""
-				"""
-				cv2.line(
-					canvas,
-					(
-						self.movAvgDict[key]["centroid"].getLastCompoundedResult().x,
-						self.movAvgDict[key]["centroid"].getLastCompoundedResult().y
-					), 
-					(
-						self.movAvgDict[key]["centroid"].getLastCompoundedResult().x + 
-						self.movAvgDict[key]["rVector"].getLastCompoundedResult().x,
-						self.movAvgDict[key]["centroid"].getLastCompoundedResult().y + 
-						self.movAvgDict[key]["rVector"].getLastCompoundedResult().y
-					),
-					255, 5
-				)
-				"""
-				cv2.circle(
-					canvas,
-					(
-						320 - 
-						self.movAvgDict[key]["rVector"].getLastCompoundedResult().x,
-						240 + 
-						self.movAvgDict[key]["rVector"].getLastCompoundedResult().y
-					),
-					5, 255, 5
-				)
-			except AttributeError:
-				pass
+	def drawCanvas(self, img):
+		canvas = np.ones_like(img)
+
+		avgX = 0
+		avgY = 0
+		sLen = len(self.subDict.keys())
+		for key in self.subDict.keys():
+			avgX += self.subDict[key]["rVector"].getLastCompoundedResult().x / sLen
+			avgY += self.subDict[key]["rVector"].getLastCompoundedResult().y / sLen
+
+		#print avgX, avgY
+		avgLookingPoint = self.Point(
+			self.tracker.xScale / 2 - avgX,
+			self.tracker.yScale / 2 + avgY
+		)
+
+		currentPoint = self.lookingPointMovAvg.compound(avgLookingPoint)
+
+		cv2.circle(canvas, currentPoint, 5, (255, 100, 100), 5)
 
 		return canvas
+
+	def updateExistingValue(self, r):
+		self.movAvgDict[r.getId()]["centroid"].compound(
+			r.getPupil().getCentroid()
+		)
+		self.movAvgDict[r.getId()]["rVector"].compound(
+			r.getResultantVector(
+				self.tracker.getXScale(), 
+				self.tracker.getYScale()
+			)
+		)
 
 	def updateMovAvgDict(self, results):
 		newDict = dict()
 		for r in results:
 			try:
-				self.movAvgDict[r.getId()]["centroid"].compound(r.getPupil().getCentroid())
-				self.movAvgDict[r.getId()]["rVector"].compound(r.getResultantVector(
-					self.tracker.getXScale(), self.tracker.getYScale()))
+				self.updateExistingValue(r)
 			except KeyError:
-				self.movAvgDict[r.getId()] = {"centroid": MovingAveragePoints(
-					r.getPupil().getCentroid(), self.movAvgLength), 
-					"rVector": MovingAveragePoints(r.getResultantVector(
-						self.tracker.getXScale(), self.tracker.getYScale()), 
-					self.movAvgLength)}
+				self.movAvgDict[r.getId()] = {
+					"centroid": 
+						MovingAveragePoints(
+							r.getPupil().getCentroid(), 
+							self.movAvgLength
+						), 
+					"rVector": 
+						MovingAveragePoints(
+							r.getResultantVector(
+								self.tracker.getXScale(), 
+								self.tracker.getYScale()
+							), 
+							self.movAvgLength
+						)
+				}
+				self.updateExistingValue(r)
+			newDict[r.getId()] = self.movAvgDict[r.getId()]
+
+		self.subDict = newDict
 
 	def run(self):
 		while True:
@@ -80,8 +90,10 @@ class EyeCalibration:
 		    #print results.idMap
 		    self.updateMovAvgDict(results)
 
-		    cv2.imshow('Canvas', self.drawCanvas())
-		    cv2.imshow('e2', results.getTrackingImage())
+		    flippedImage = cv2.flip(results.getTrackingImage(), 1)
+
+		    cv2.imshow('Canvas', self.drawCanvas(flippedImage))
+		    cv2.imshow('e2', flippedImage)
 		    if cv2.waitKey(1) == 27:
 		        break
 		cv2.destroyAllWindows()